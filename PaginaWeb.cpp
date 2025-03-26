3b58780cfec109374153e779fd531661cd3b6611
SantiagoSierra62 <santiagosierra0602@gmail.com>
2025-03-26 15:43:57 -0300

    Fue

--- PaginaWeb.cpp
+++
@@ -1,9 +0,0 @@
-#include "PaginaWeb.h"
-
-using namespace std;
-
-PaginaWeb::PaginaWeb(const string& d, const string& t, const DTFecha& f, const string& u, const string& c) : Publicacion(d, t, f), url(u), contenidoExtraido(c) {}
-
-bool PaginaWeb::contienePalabra(const string& palabra) {
-    
-}
