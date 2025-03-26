5b69406116c673f61e65be837d430900c3876f65
SantiagoSierra62 <santiagosierra0602@gmail.com>
2025-03-26 15:49:24 -0300

    Todos los .h

---
+++ include/Publicacion.h
@@ -0,0 +1,46 @@
+#ifndef PUBLICACION_H
+#define PUBLICACION_H
+
+#include <string>
+#include <map>
+#include "DTRefer.h"
+#include "DTFecha.h"
+#include "Investigador.h"
+
+class Investigador;
+
+class Publicacion{
+    protected:
+        const std::string DOI;        //DOI std::string
+        std::string titulo;          //titulo std::string
+        DTFecha fecha;          //datatype DTFecha fecha
+        std::map<std::string, Investigador*> investigadores;
+    public:
+        //Constructor
+        Publicacion(std::string DOI, std::string titulo, DTFecha fecha);
+
+        //Metodos virtuales puros los dejo a ver
+
+        //Metodos og
+        //getters
+
+        virtual std::string getDOI();
+        virtual std::string getTitulo();
+        virtual DTFecha getFecha();
+        //setters
+        virtual void setTitulo(std::string Titulo);
+        virtual void setFecha(DTFecha Fecha);
+
+        //funciones que vienen del DCD
+        DTRefer getDT();
+
+        virtual void agregarAutor(Investigador* investigador); //ponemos los virtual en estos por las dudas xd
+        virtual void eliminarAutor(Investigador* investigador);
+
+        //metodos necesariamente virtuales
+        virtual bool contienePalabra(const std::string& palabra) const= 0;
+        virtual ~Publicacion() = 0;                         //Destructor virtual
+
+};
+
+#endif
