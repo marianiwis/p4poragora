4b4b4fc2b92cabef45636d6c4bd28d8b7fb09b5e
SantiagoSierra62 <santiagosierra0602@gmail.com>
2025-03-26 17:11:16 -0300

    Merge Dev

---
+++ include/Investigador.h
@@ -0,0 +1,39 @@
+
+#ifndef INVESTIGADOR_H
+#define INVESTIGADOR_H
+
+#include <string>
+#include <map>
+
+#include <set>
+#include "DTFecha.h"
+#include "Publicacion.h"
+
+
+class Publicacion;
+
+using namespace std;
+//el const en public hace que las funciones sean constantes no sus resultados
+ 
+class Investigador{
+    private:
+        const string ORCID;
+        string nombre, institucion;
+        map<string, Publicacion*> publicaciones;
+    public:
+        Investigador(const string& ORCID, const string& nombre, const string& institucion);
+        ~Investigador();
+
+        string getORCID() const;
+        string getNombre() const;
+        string getInstitucion() const;
+
+        void agregarPublicacion(Publicacion* pub);
+        void eliminarPublicacion(Publicacion* pub);
+
+        string toString() const;
+        set<string> listarPublicaciones(const DTFecha& desde, const string& palabra);
+};
+
+
+#endif
---
+++ src/ArticuloRevista.cpp
@@ -0,0 +1,10 @@
+
+#include "ArticuloRevista.h"
+
+using namespace std;
+
+ArticuloRevista::ArticuloRevista(const string& DOI, const string& titulo, const DTFecha& fecha, const string& revista, const string& extracto) : Publicacion(DOI, titulo, fecha), revista(revista), extracto(extracto){}
+
+bool ArticuloRevista::contienePalabra(const string& palabra) const{
+    return extracto.find(palabra) != string::npos;
+}
