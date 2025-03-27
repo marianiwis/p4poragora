417c832b0c39722442a602305252ac21fab31899
joaquina-garcia <joaquigarcia2003@gmail.com>
2025-03-27 14:27:28 -0300

    cambios en libro y 6g-h

--- src/Libro.cpp
+++ src/Libro.cpp
@@ -5,9 +5,10 @@ using namespace std;
 Libro::Libro(const string& DOI, const string& titulo, const DTFecha& fecha, const string& editorial, const set<string>& keyWords) : Publicacion(DOI, titulo, fecha), editorial(editorial), palabrasDestacadas(keyWords){}
 
 bool Libro::contienePalabra(const string& palabra) const{
-    set<string>::const_iterator it; //const iterator para asegurar no modificar el set
+    /*set<string>::const_iterator it; //const iterator para asegurar no modificar el set
     for (it = palabrasDestacadas.begin(); it != palabrasDestacadas.end(); ++it)
         if (*it == palabra) 
             return true;
-    return false;
+    return false;*/
+    return palabrasDestacadas.find(palabra) != palabrasDestacadas.end();
 }
